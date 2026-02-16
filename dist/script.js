8a865c3652d96f8323be519c6afda1bc481dce6b
bruno <bpires.siqueira@gmail.com>
2026-02-16 20:22:56 -0300

    feat: normalizar transação

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,11 @@
 import fetchData from "./fetchData.js";
+import normalizarTransacao from "./normalizarTransacao.js";
 async function handleData() {
-    const data = await fetchData("https://api.origamid.dev/json/transacoess.json");
-    if (data) {
-        data.forEach((item) => {
-            console.log(item["Cliente Novo"]);
-        });
-    }
+    const data = await fetchData("https://api.origamid.dev/json/transacoes.json");
+    if (!data)
+        return;
+    const transacoes = data.map(normalizarTransacao);
+    console.log(transacoes);
 }
 handleData();
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,32 +1,12 @@
 import fetchData from "./fetchData.js";
-
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus =
-  | "Paga"
-  | "Recusada pela operadora de cartão"
-  | "Aguardando pagamento"
-  | "Estornada";
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ["Valor (R$)"]: string;
-  ["Forma de Pagamento"]: TransacaoPagamento;
-  ["Cliente Novo"]: number;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
-    "https://api.origamid.dev/json/transacoess.json",
+    "https://api.origamid.dev/json/transacoes.json",
   );
-  if (data) {
-    data.forEach((item) => {
-      console.log(item["Cliente Novo"]);
-    });
-  }
+  if (!data) return;
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
 }
-
 handleData();
