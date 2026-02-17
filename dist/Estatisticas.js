9aad19379043e58e0eed3e30bcb5ed5c08829ac4
bruno <bpires.siqueira@gmail.com>
2026-02-17 15:55:00 -0300

    feat: melhor dia

--- dist/Estatisticas.js
+++ dist/Estatisticas.js
@@ -7,11 +7,15 @@ export default class Estatisticas {
     total;
     pagamento;
     status;
+    semana;
+    melhorDia;
     constructor(transacoes) {
         this.transacoes = transacoes;
         this.total = this.setTotal();
         this.pagamento = this.setPagamento();
         this.status = this.setStatus();
+        this.semana = this.setSemana();
+        this.melhorDia = this.setMelhorDia();
     }
     setTotal() {
         return this.transacoes.filter(filtrarValor).reduce((acc, item) => {
@@ -23,6 +27,40 @@ export default class Estatisticas {
     }
     setStatus() {
         return countBy(this.transacoes.map(({ status }) => status));
+    }
+    setSemana() {
+        const semana = {
+            ["Domingo"]: 0,
+            ["Segunda"]: 1,
+            ["Terça"]: 2,
+            ["Quarta"]: 3,
+            ["Quinta"]: 4,
+            ["Sexta"]: 5,
+            ["Sabado"]: 6,
+        };
+        for (let i = 0; i < this.transacoes.length; i++) {
+            const day = this.transacoes[i].data.getDay();
+            if (day === 0)
+                semana["Domingo"] += 1;
+            if (day === 1)
+                semana["Segunda"] += 1;
+            if (day === 2)
+                semana["Terça"] += 1;
+            if (day === 3)
+                semana["Quarta"] += 1;
+            if (day === 4)
+                semana["Quinta"] += 1;
+            if (day === 5)
+                semana["Sexta"] += 1;
+            if (day === 6)
+                semana["Sabado"] += 1;
+        }
+        return semana;
+    }
+    setMelhorDia() {
+        return Object.entries(this.semana).sort((a, b) => {
+            return b[1] - a[1];
+        })[0];
     }
 }
 //# sourceMappingURL=Estatisticas.js.map
